828dabe305e6d0c44047e194f87e7eb5bf8a0bd0
Joao Monteiro <joao.monteiro@ahgora.com.br>
2019-07-18 14:19:36 -0300

    Ignore when topic not exist

--- src/Lookup/Lookup.php
+++ src/Lookup/Lookup.php
@@ -59,8 +59,8 @@ class Lookup
         $lookupHosts = [];
         $topicChannel = [];
 
-        foreach ($this->hosts as $item) {
-            $url = sprintf('http://%s/lookup?topic=%s', $item, urlencode($topic));
+        foreach ($this->hosts as $hostUrl) {
+            $url = sprintf('http://%s/lookup?topic=%s', $hostUrl, urlencode($topic));
 
             $ch = curl_init($url);
             $options = [
@@ -75,10 +75,7 @@ class Lookup
             ];
 
             curl_setopt_array($ch, $options);
-            if (!$resultStr = curl_exec($ch)) {
-                throw new LookupException('Error talking to nsq lookupd via ' . $url);
-            }
-
+            $resultStr = curl_exec($ch);
             if (!curl_error($ch) && curl_getinfo($ch, CURLINFO_HTTP_CODE) == '200') {
                 $result = json_decode($resultStr, true);
                 $producers = [];
@@ -104,13 +101,15 @@ class Lookup
                     }
                 }
                 curl_close($ch);
+            } elseif (curl_getinfo($ch, CURLINFO_HTTP_CODE) == '404') {
+                Log::info("Topic not found at URL: $url");
+                continue;
             } else {
                 $err = curl_error($ch);
                 Log::error($err . $resultStr);
                 curl_close($ch);
                 throw new LookupException($err, -1);
             }
-
         }
 
         return [
