2af41023ba935c68b5377a9ad62948033f9f5085
Joao Monteiro <joao.monteiro@ahgora.com.br>
2021-02-22 10:43:29 -0300

    Rollback

--- src/Model/Nsqd.php
+++ src/Model/Nsqd.php
@@ -74,12 +74,12 @@ class Nsqd extends AbstractMonitor
         // send identify params
         Log::debug('send identify params');
         $this->client->send(Packet::identify(Arr::get($this->nsqDriverConfig, 'identify')));
-        // $frame = Unpack::getFrame($this->receive());
-        // if (!Unpack::isOk($frame)) {
-        //     throw new IdentifyException("Something is wrong to send IDENTIFY message to " . 
-        //         $this->getTcpAddress() . 
-        //         ". Response: ". json_encode($frame));
-        // }
+        $frame = Unpack::getFrame($this->receive());
+        if (!Unpack::isOk($frame)) {
+            throw new IdentifyException("Something is wrong to send IDENTIFY message to " . 
+                $this->getTcpAddress() . 
+                ". Response: ". json_encode($frame));
+        }
     }
 
     public function sub()
@@ -87,12 +87,12 @@ class Nsqd extends AbstractMonitor
         // sub nsq topic and channel
         Log::debug('sub nsq topic and channel');
         $this->client->send(Packet::sub($this->topic, $this->channel));
-        // $frame = Unpack::getFrame($this->receive());
-        // if (!Unpack::isOk($frame)) {
-        //     throw new SubscribeException("Something is wrong to send SUB message to " . 
-        //         $this->getTcpAddress() . 
-        //         ". Response: ". json_encode($frame));
-        // }
+        $frame = Unpack::getFrame($this->receive());
+        if (!Unpack::isOk($frame)) {
+            throw new SubscribeException("Something is wrong to send SUB message to " . 
+                $this->getTcpAddress() . 
+                ". Response: ". json_encode($frame));
+        }
         
         $this->rdy(Arr::get($this->nsqDriverConfig, 'options.rdy', 1));
 
